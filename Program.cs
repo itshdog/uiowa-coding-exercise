9849433071334a5ca592d45cf9d697bdd6718213
itshdog <haydenwestphal@gmail.com>
2023-08-08 19:10:24 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace codingExercise {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
